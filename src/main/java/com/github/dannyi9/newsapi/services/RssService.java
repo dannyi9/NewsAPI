02741b2f97a9ac60fbff6ad483ed43291f56c591
dannyi9 <danyaalislam@yahoo.co.uk>
2025-02-15 17:50:32 +0000

    Added rome implementation for RSS feed parsing, added basic print titles of RSS news articles

--- src/main/java/com/github/dannyi9/newsapi/services/RssService.java
+++ src/main/java/com/github/dannyi9/newsapi/services/RssService.java
@@ -1,7 +1,15 @@
 package com.github.dannyi9.newsapi.services;
 
+import java.net.URL;
+import java.util.List;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
 
 @Service
 public class RssService {
@@ -14,6 +22,24 @@ public class RssService {
     
     private void parseRssFeed() {
         System.out.println("Parsing RSS feed...");
+
+        try {
+            String rssFeedUrl = "https://feeds.skynews.com/feeds/rss/uk.xml";
+
+            URL url = new URL(rssFeedUrl);
+            
+            SyndFeedInput input = new SyndFeedInput();
+            SyndFeed feed = input.build(new XmlReader(url));
+
+            List<SyndEntry> entries = feed.getEntries();
+
+            for (SyndEntry entry : entries) {
+                System.out.println("Title: " + entry.getTitle());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
